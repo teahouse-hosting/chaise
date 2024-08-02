c5d11f7fbe801d225fccc690f88b0f1f2f8a5639
Jamie Bliss <jamie@ivyleav.es>
2024-08-02 15:07:36 -0400

    Add apply command

--- src/chaise/cli/__init__.py
+++ src/chaise/cli/__init__.py
@@ -3,16 +3,43 @@ CouchDB management tool
 """
 
 import argparse
+import logging
 
 
-parser = argparse.ArgumentParser(
-    prog="chaise",
-    description=__doc__,
-)
+def apply(args):
+    """
+    Apply the given database descriptions
+    """
+    print("TODO: apply")
+
+
+def _arg_parser():
+    parser = argparse.ArgumentParser(
+        prog="chaise",
+        description=__doc__,
+    )
+    parser.add_argument(
+        "--verbose", action="store_true", help="Enable verbose logging."
+    )
+    parser.set_defaults(func=lambda args: parser.print_usage())
+
+    subparsers = parser.add_subparsers(title="Subcommands")
+
+    applyp = subparsers.add_parser("apply", help=apply.__doc__)
+    applyp.set_defaults(func=apply)
+
+    return parser
 
 
 def main():
-    _ = parser.parse_args()
+    args = _arg_parser().parse_args()
+
+    logging.basicConfig(
+        level=logging.INFO if args.verbose else logging.WARNING,
+        format="%(name)s %(levelname)s %(message)s",
+    )
+
+    args.func(args)
 
 
 if __name__ == "__main__":
