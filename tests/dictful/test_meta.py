b87626d757feddf31618719463eeaca5496ea2e7
Jamie Bliss <jamie@ivyleav.es>
2024-08-27 14:29:12 -0400

    Fix test_list_dbs
    It's pretty inconsistent if system databases will be ready

--- tests/dictful/test_meta.py
+++ tests/dictful/test_meta.py
@@ -11,6 +11,7 @@ pytestmark = pytest.mark.anyio
 async def test_list_dbs(basic_session, basic_database):
     all_dbs = set()
     async for dbname in basic_session.iter_dbs():
-        all_dbs.add(dbname)
+        if not dbname.startswith("_"):
+            all_dbs.add(dbname)
 
-    assert all_dbs == {"_users", "_replicator", basic_database._name}
+    assert all_dbs == {basic_database._name}
