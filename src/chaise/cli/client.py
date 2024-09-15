f8d8a36b8c89961c6141b953de7824c94886b774
Jamie Bliss <jamie@ivyleav.es>
2024-09-14 20:50:48 -0400

    Use helpers in CLI

--- src/chaise/cli/client.py
+++ src/chaise/cli/client.py
@@ -1,10 +1,6 @@
 import chaise.dictful
+import chaise.helpers
 
 
-class ConstantPool(chaise.dictful.BasicPool):
-    def __init__(self, url: str):
-        self.url = url
-        super().__init__()
-
-    async def iter_servers(self):
-        yield self.url
+class ConstantPool(chaise.helpers.ConstantPoolMixin, chaise.dictful.BasicPool):
+    pass
