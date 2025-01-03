daff2d0d23d94b56b7afb43654eee2477daeb19c
Jamie Bliss <jamie@ivyleav.es>
2025-01-03 16:56:31 -0500

    Update tests

--- tests/cli/test_apply.py
+++ tests/cli/test_apply.py
@@ -6,5 +6,5 @@ pytestmark = pytest.mark.anyio
 
 async def test_apply(cli, cli_session):
     await cli("apply", "demo_schema")
-    assert await cli_session.get_db("db1")
-    assert await cli_session.get_db("db2")
+    dbs = {db async for db in cli_session.iter_dbs() if not db.startswith("_")}
+    assert dbs == {"db-empty", "db-nearly-empty", "db-realname"}
