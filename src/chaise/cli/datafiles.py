c0cb36f6c56fea15194dea0f6b075361203c992d
Jamie Bliss <jamie@ivyleav.es>
2025-01-03 19:55:35 -0500

    Add index parsing

--- src/chaise/cli/datafiles.py
+++ src/chaise/cli/datafiles.py
@@ -15,19 +15,37 @@ class ParseError(Exception):
 
 
 @dataclasses.dataclass
+class Index:
+    name: str
+    fields: list[str]
+    filter: dict | None = None
+
+
+@dataclasses.dataclass
+class DesignDoc:
+    name: str
+    _resource: importlib.resources.abc.Traversable
+    indexes: list[Index]
+
+
+@dataclasses.dataclass
 class DeclaredDB:
     name: str
     _resource: importlib.resources.abc.Traversable
     # options: ...
+    ddocs: list[DesignDoc]
 
 
 class Loader:
-    def load_document(self, file_contents: str, db: DeclaredDB):
+    def load_database(self, file_contents: str, db: DeclaredDB):
+        raise NotImplementedError
+
+    def load_ddoc(self, file_contents: str, ddoc: DesignDoc):
         raise NotImplementedError
 
 
 class KdlLoader:
-    def load_document(self, file_contents: str, db: DeclaredDB):
+    def load_database(self, file_contents: str, db: DeclaredDB):
         if not file_contents.strip():
             return
         doc = ckdl.parse(file_contents, version="detect")
@@ -41,7 +59,41 @@ class KdlLoader:
             case (name,):
                 db.name = name
             case _:
-                raise ParseError("Invalid KDL: Unexpected arguments")
+                raise ParseError(f"Invalid KDL: Unexpected arguments: {node}")
+
+    def load_ddoc(self, file_contents: str, ddoc: DesignDoc):
+        if not file_contents.strip():
+            return
+        doc = ckdl.parse(file_contents, version="detect")
+
+        for node in doc.nodes:
+            match node.name:
+                case "index":
+                    ddoc.indexes.append(self._parse_index(node))
+                case _:
+                    raise ParseError(f"Uknown node: {node}")
+
+    def _parse_index(self, node) -> Index:
+        match node.args:
+            case ():
+                raise ParseError(f"Invalid KDL: Expecting arguments: {node}")
+            case (name,):
+                index_name = name
+            case _:
+                raise ParseError(f"Invalid KDL: Unexpected arguments: {node}")
+
+        index_fields = []
+        for child in node.children:
+            match child.name:
+                case "field" | "fields":
+                    index_fields += child.args
+                case _:
+                    raise ParseError(f"Invalid KDL: Uknown node: {child}")
+
+        return Index(
+            name=index_name,
+            fields=index_fields,
+        )
 
 
 LOADERS: dict[str, type[Loader]] = {
@@ -67,6 +119,8 @@ def walk(anchor):
                     n = posixpath.join(base, t.name)
                 else:
                     n = t.name
+                if not hasattr(t, "parent"):
+                    t.parent = trav
                 yield from _recurse(n, t)
 
     yield from _recurse("", importlib.resources.files(anchor))
@@ -87,6 +141,31 @@ def find_dbs(anchor) -> Iterable[DeclaredDB]:
                 obj = DeclaredDB(
                     name=posixpath.basename(parent),
                     _resource=t,
+                    ddocs=[],
                 )
-                loader.load_document(t.read_text(), obj)
+                loader.load_database(t.read_text(), obj)
+                obj.ddocs += list(find_ddocs(t.parent))
                 yield obj
+
+
+def find_ddocs(trav):
+    assert trav.is_dir()
+    for file in trav.iterdir():
+        if file.is_dir():
+            continue
+        stem, ext = posixpath.splitext(file.name)
+        if stem == "__db__":
+            continue
+        try:
+            loader = LOADERS[ext]()
+        except KeyError as exc:
+            # TODO: emit warning
+            exc
+        else:
+            ddoc = DesignDoc(
+                name=stem,
+                _resource=file,
+                indexes=[],
+            )
+            loader.load_ddoc(file.read_text(), ddoc)
+            yield ddoc
