00ea3387bd2c0a0bae17adf18b724876b93370ae
Jamie Bliss <jamie@ivyleav.es>
2025-02-14 17:47:15 -0500

    Fix test_apply_testing

--- tests/cli/test_apply.py
+++ tests/cli/test_apply.py
@@ -24,8 +24,7 @@ async def test_apply_testing(couch_url, cli_session):
     import chaise.testing
 
     async with chaise.testing.run_cli_apply(couch_url, "demo_schema"):
-        assert await cli_session.get_db("db1")
-        assert await cli_session.get_db("db2")
+        assert await cli_session.get_db("db-empty")
 
     with pytest.raises(chaise.Missing):
-        await cli_session.get_db("db1")
+        await cli_session.get_db("db-empty")
