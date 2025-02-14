be3203f47451fe36f613054931760f56db841f66
Jamie Bliss <jamie@ivyleav.es>
2025-02-14 15:57:51 -0500

    Add some testing to run_cli_apply()

--- tests/cli/test_apply.py
+++ tests/cli/test_apply.py
@@ -8,3 +8,14 @@ async def test_apply(cli, cli_session):
     await cli("apply", "demo_schema")
     assert await cli_session.get_db("db1")
     assert await cli_session.get_db("db2")
+
+
+async def test_apply_testing(couch_url, cli_session):
+    import chaise.testing
+
+    async with chaise.testing.run_cli_apply(couch_url, "demo_schema"):
+        assert await cli_session.get_db("db1")
+        assert await cli_session.get_db("db2")
+
+    with pytest.raises(chaise.Missing):
+        await cli_session.get_db("db1")
